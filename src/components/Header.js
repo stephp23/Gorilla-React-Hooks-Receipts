4152463087555021f485a5b7e9cc5150f944d668
stephp23 <stephp23@gmail.com>
2021-01-22 04:14:07 -0500

    css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
 const Header = () => {
   return (
     <div>
-      <h1>Gorilla</h1>
-      <h2>Korean BBQ</h2>
+      <h1>Korilla</h1>
     </div>
   )
 }
--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -2,34 +2,21 @@
 import "./Receipt.css";
 
 function Receipt({ person, ...receiptsDetails }) {
-  // const [toggle, setToggle] = useState(paid);
-  // // first need to check if the order is paid or unpaid, 
-  // // before showing the final receipt on the webpage.
-  // function paymentStatus() {
-  //   // compares data in array for paid status (true or false),
-  //   // and shows "Paid" for true value, and "Not Paid" for false value.
-  //   setToggle((payment) => (payment ? "Paid" : "Not Paid")); 
-  //}
 
   return (
     <div className="receipt-container">
-      <h3>Customer: {(person)}</h3>
+      <h3>{(person)}</h3>
         <ul>
-            <li><span class="receipt-categories">Main:</span> {receiptsDetails.order.main}</li>
-            <li><span class="receipt-categories">Protein:</span> {receiptsDetails.order.protein}</li>
-            <li><span class="receipt-categories">Rice:</span> {receiptsDetails.order.rice}</li>
-            <li><span class="receipt-categories">Sauce:</span> {receiptsDetails.order.sauce}</li>
-            <li><span class="receipt-categories">Drink:</span> {receiptsDetails.order.drink}</li>
-            <li><span class="receipt-categories">Toppings:</span> {receiptsDetails.order.toppings.map((topping, index) => {
-              return `${ topping } , `;
+            <li class="receipt-info"><span class="receipt-categories">Main:</span>{receiptsDetails.order.main}</li>
+            <li class="receipt-info"><span class="receipt-categories">Protein:</span> {receiptsDetails.order.protein}</li>
+            <li class="receipt-info"><span class="receipt-categories">Rice:</span> {receiptsDetails.order.rice}</li>
+            <li class="receipt-info"><span class="receipt-categories">Sauce:</span> {receiptsDetails.order.sauce}</li>
+            <li class="receipt-info"><span class="receipt-categories">Drink:</span> {receiptsDetails.order.drink}</li>
+            <li class="receipt-info"><span class="receipt-categories">Toppings:</span> {receiptsDetails.order.toppings.map((topping, index) => {
+              return ` ${ topping } . `;
                 })}</li>
-            <li><span class="receipt-categories">Cost:</span> {`$`}{receiptsDetails.order.cost} </li>
+            <li class="receipt-info"><span class="receipt-categories">Cost:</span> {`$`}{receiptsDetails.order.cost} </li>
         </ul>
-    
-    {/* <div className="receiptPayment-container">
-        <button onClick={ paymentStatus }>Payment Status</button>
-        <h3>{ toggle }</h3>
-    </div> */}
     </div>
 );
 }
