a1a6bd4176b9f96ece213ebac7a3e0aafc2b2dc9
stephp23 <stephp23@gmail.com>
2021-01-22 03:15:59 -0500

    fixed issue with receipt data retrieval

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import receipts from "../data/recordsReceipts";
+//import { useState } from "react";
 import "./Receipt.css";
 
 function Receipt({ person, ...receiptsDetails }) {
-  const [toggle, setToggle] = useState(paid);
-  // first need to check if the order is paid or unpaid, 
-  // before showing the final receipt on the webpage.
-  function paymentStatus() {
-    // compares data in array for paid status (true or false),
-    // and shows "Paid" for true value, and "Not Paid" for false value.
-    setToggle((payment) => (payment ? "Paid" : "Not Paid")); 
-  }
+  // const [toggle, setToggle] = useState(paid);
+  // // first need to check if the order is paid or unpaid, 
+  // // before showing the final receipt on the webpage.
+  // function paymentStatus() {
+  //   // compares data in array for paid status (true or false),
+  //   // and shows "Paid" for true value, and "Not Paid" for false value.
+  //   setToggle((payment) => (payment ? "Paid" : "Not Paid")); 
+  //}
 
   return (
     <div className="receipt-container">
       <h3>Customer: {(person)}</h3>
-      <ul>
-          <li><span class="receipt-categories">Main:</span> {receiptsDetails.order.main}</li>
-          <li><span class="receipt-categories">Protein:</span> {receiptsDetails.order.protein}</li>
-          <li><span class="receipt-categories">Rice:</span> {receiptsDetails.order.rice}</li>
-          <li><span class="receipt-categories">Sauce:</span> {receiptsDetails.order.sauce}</li>
-          <li><span class="receipt-categories">Drink:</span> {receiptsDetails.order.drink}</li>
-          <li><span class="receipt-categories">Toppings:</span> {receiptsDetails.order.toppings}</li>
-      </ul>
+        <ul>
+            <li><span class="receipt-categories">Main:</span> {receiptsDetails.order.main}</li>
+            <li><span class="receipt-categories">Protein:</span> {receiptsDetails.order.protein}</li>
+            <li><span class="receipt-categories">Rice:</span> {receiptsDetails.order.rice}</li>
+            <li><span class="receipt-categories">Sauce:</span> {receiptsDetails.order.sauce}</li>
+            <li><span class="receipt-categories">Drink:</span> {receiptsDetails.order.drink}</li>
+            <li><span class="receipt-categories">Toppings:</span> {receiptsDetails.order.toppings}</li>
+            <li><span class="receipt-categories">Cost:</span> {`$`} {receiptsDetails.order.cost} </li>
+        </ul>
     
-      <div className="receiptPayment-container">
-        <button onClick={ payment }>Payment Status</button>
+    {/* <div className="receiptPayment-container">
+        <button onClick={ paymentStatus }>Payment Status</button>
         <h3>{ toggle }</h3>
-      </div>
+    </div> */}
     </div>
 );
 }
